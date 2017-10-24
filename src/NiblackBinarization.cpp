92a52584ef9e135d29eefc8d7e0c61db620f2e09
KojiKobayashi <k.kobayashi@33i.co.jp>
2017-10-24 18:45:50 +0900

    箱作成

---
+++ src/NiblackBinarization.cpp
@@ -0,0 +1,4 @@
+#include "NiblackBinarization.h"
+
+namespace ImageBinarization {
+}
---
+++ src/NiblackBinarization.h
@@ -0,0 +1,11 @@
+#ifndef _NIBLACK_BINARIZATION_H_
+#define _NIBLACK_BINARIZATION_H_
+#pragma once
+
+namespace ImageBinarization
+{
+    class NiblackBinarization
+    {
+    };
+}
+#endif // _NIBLACK_BINARIZATION_H_
